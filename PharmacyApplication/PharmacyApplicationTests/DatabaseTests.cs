f7e5e4505c6a86ccfed3bea94ce6c7b151b82891
Jacques Van Niekerk <9508554@student.swin.edu.au>
2018-04-10 19:16:35 +1000

    added Tests to Solution

---
+++ PharmacyApplication/PharmacyApplicationTests/DatabaseTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PharmacyApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApplication.Tests
+{
+    [TestClass()]
+    public class DatabaseTests
+    {
+        [TestMethod()]
+        public void CreateTableTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
