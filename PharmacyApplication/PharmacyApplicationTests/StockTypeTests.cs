1b14aba633cf7a98284772980b9cb6fb0f155af9
Jacques Van Niekerk <jshark46@gmail.com>
2018-04-15 15:03:52 +1000

    REFACTOR: Speraited the test File in Class tests

---
+++ PharmacyApplication/PharmacyApplicationTests/StockTypeTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PharmacyApplication;
+
+namespace PharmacyApplicationTests
+{
+    /// <summary>
+    /// Summary description for StockTypeTests
+    /// </summary>
+    [TestClass]
+    public class StockTypeTests
+    {
+        [TestMethod()]
+        public void StockTypeInitialisationSucceed()
+        {
+            int id = 700;
+            string name = "berries";
+            int level = 40;
+
+            StockType toTest = new StockType(id, name, level);
+
+            /*Assert.AreEqual(id, toTest.ID);
+            Assert.AreEqual(name, toTest.Name);
+            Assert.AreEqual(level, toTest.Level);//*/
+        }
+
+        [TestMethod()]
+        public void StockTypeInitialisationFailID()
+        {
+            int id = 700;
+            string name = "berries";
+            int level = 40;
+
+            StockType toTest = new StockType(id, name, level);
+
+            toTest.ID -= 50;
+
+            Assert.AreNotEqual(id, toTest.ID);
+            Assert.AreEqual(name, toTest.Name);
+            Assert.AreEqual(level, toTest.Level);
+        }
+
+        [TestMethod()]
+        public void StockTypeInitialisationFailName()
+        {
+            int id = 700;
+            string name = "berries";
+            int level = 40;
+
+            StockType toTest = new StockType(id, name, level);
+
+            toTest.Name = "BlueBerries";
+
+            Assert.AreEqual(id, toTest.ID);
+            Assert.AreNotEqual(name, toTest.Name);
+            Assert.AreEqual(level, toTest.Level);
+        }
+
+        [TestMethod()]
+        public void StockTypeInitialisationFailLevelIncrement()
+        {
+            int id = 700;
+            string name = "berries";
+            int level = 40;
+
+            StockType toTest = new StockType(id, name, level);
+
+            toTest.Level += 1;
+
+            Assert.AreEqual(id, toTest.ID);
+            Assert.AreEqual(name, toTest.Name);
+            Assert.AreNotEqual(level, toTest.Level);
+        }
+
+        [TestMethod()]
+        public void StockTypeInitialisationFailLevelDecrement()
+        {
+            int id = 700;
+            string name = "berries";
+            int level = 40;
+
+            StockType toTest = new StockType(id, name, level);
+
+            toTest.Level -= 1;
+
+            Assert.AreEqual(id, toTest.ID);
+            Assert.AreEqual(name, toTest.Name);
+            Assert.AreNotEqual(level, toTest.Level);
+        }
+    }
+}
