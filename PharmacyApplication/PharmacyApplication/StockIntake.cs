6176d727c4957699eaa86d67e6cdf233e6d1d230
jed <jaehad10@gmail.com>
2018-04-24 18:00:44 +1000

    Stock intake added

---
+++ PharmacyApplication/PharmacyApplication/StockIntake.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApplication
+{
+    //Authors: Jed
+    /// <summary>
+    /// Represents a stock intake, designed to store values when loaded out of memory
+    /// </summary>
+    public class StockIntake : DBReadable
+    {
+        //For IDBReadable
+        protected override Type[] FieldTypesToRead
+        {
+            get
+            {
+                return new Type[3] { typeof(string), typeof(int), typeof(int) };
+            }
+        }
+
+        /// <summary>
+        /// Represnts the date on which the intake occured
+        /// </summary>
+        //Elements 0
+        public string Date
+        {
+            get
+            {
+                return (string)_elements[0];//TODO update to use FieldTypesToRead instead of hard cast to int.
+            }
+
+            set
+            {
+                _elements[0] = value;
+            }
+        }
+
+        /// <summary>
+        /// Represents a unique identifier for the stock type in-taken
+        /// </summary>
+        //Elements 1
+        public int ID
+        {
+            get
+            {
+                return (int)_elements[1];//TODO update to use FieldTypesToRead instead of hard cast to int.
+            }
+
+            set
+            {
+                _elements[1] = value;
+            }
+        }
+
+        /// <summary>
+        /// Represents the number of stock in-taken.
+        /// </summary>
+        //Elements 2
+        public int Amount
+        {
+            get
+            {
+                return (int)_elements[2];//TODO update to use FieldTypesToRead instead of hard cast to int.
+            }
+
+            set
+            {
+                _elements[2] = value;
+            }
+        }
+
+
+        public StockIntake(string date, int id, int amount) : base()
+        {
+            this.ID = id;
+            this.Date = date;
+            this.Amount = amount;
+        }
+
+        public StockIntake(object[] objs) : base()
+        {
+            this.ReadFromGeneric(objs);
+        }
+    }
+}
---
+++ PharmacyApplication/PharmacyApplicationTests/StockIntakeTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PharmacyApplication;
+
+namespace PharmacyApplicationTests
+{
+    /// <summary>
+    /// Summary description for StockTypeTests
+    /// </summary>
+    [TestClass]
+    public class StockIntakeTests
+    {
+        [TestMethod()]
+        public void StockIntakeInitialisationSucceed()
+        {
+            string date = "01-02-1997";
+            int id = 700;
+            int amount = 5;
+
+            StockIntake toTest = new StockIntake(date, id, amount);
+
+            Assert.AreEqual(date, toTest.Date);
+            Assert.AreEqual(id, toTest.ID);
+            Assert.AreEqual(amount, toTest.Amount);
+        }
+
+        [TestMethod()]
+        public void StockTypeInitialisationFailDate()
+        {
+            string date = "01-02-1997";
+            int id = 700;
+            int amount = 5;
+
+            StockIntake toTest = new StockIntake(date, id, amount);
+
+            toTest.Date = "26-07-1998";
+
+            Assert.AreNotEqual(date, toTest.Date);
+            Assert.AreEqual(id, toTest.ID);
+            Assert.AreEqual(amount, toTest.Amount);
+        }
+
+        [TestMethod()]
+        public void StockTypeInitialisationFailID()
+        {
+            string date = "01-02-1997";
+            int id = 700;
+            int amount = 5;
+
+            StockIntake toTest = new StockIntake(date, id, amount);
+
+            toTest.ID -= 5;
+
+            Assert.AreEqual(date, toTest.Date);
+            Assert.AreNotEqual(id, toTest.ID);
+            Assert.AreEqual(amount, toTest.Amount);
+        }
+
+        [TestMethod()]
+        public void StockTypeInitialisationFailAmount()
+        {
+            string date = "01-02-1997";
+            int id = 700;
+            int amount = 5;
+
+            StockIntake toTest = new StockIntake(date, id, amount);
+
+            toTest.Amount += 20;
+
+            Assert.AreEqual(date, toTest.Date);
+            Assert.AreEqual(id, toTest.ID);
+            Assert.AreNotEqual(amount, toTest.Amount);
+        }
+    }
+}
